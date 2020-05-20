f3c8f5d22f5ee2d02321b65d5c0044722d61f2ba
Joseph Romero <joeromero47@gmail.com>
2020-05-20 19:45:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,40 +1,35 @@
-require 'pry'
 class Triangle
-  def initialize(side_1, side_2, side_3)
-    @triangle_sides = []
-    @triangle_sides << side_1
-    @triangle_sides << side_2
-    @triangle_sides << side_3
+  
+  attr_accessor :s1, :s2, :s3, :kind
+  
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end 
+  
+  def kind 
+    if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 || (s3 + s1) <= s2
+     begin
+    raise TriangleError
+    puts error.message
   end
-
-  def valid?
-    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
-    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
-    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
-
-    if (@triangle_sides.none? {|side| side <= 0}) &&
-      (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
-      return true
-    else
-      return false
-    end
+  
+  elsif s1 == s2 && s1 == s3
+  self.kind = :equilateral
+  
+  elsif s1 == s2 || s1 == s3 || s2 == s3
+  self.kind = :isosceles
+  
+ else 
+   self.kind = :scalene
+  
+    end   
   end
+end 
+    
 
-  def kind
-    if valid?
-      if @triangle_sides.uniq.length == 1
-        return :equilateral
-      elsif @triangle_sides.uniq.length == 2
-        return :isosceles
-      else
-        return :scalene
-      end
-    else
-      raise TriangleError
-    end
-  end
-end
 
-class TriangleError < StandardError
-
+  class TriangleError < StandardError
+    
 end
