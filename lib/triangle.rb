216a9304c0f264ee1640f162a3cd39fccd487c90
Joseph Romero <joeromero47@gmail.com>
2020-05-20 19:46:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,16 +13,15 @@ class Triangle
      begin
     raise TriangleError
     puts error.message
-  end
   
-  elsif s1 == s2 && s1 == s3
-  self.kind = :equilateral
+    elsif s1 == s2 && s1 == s3
+      self.kind = :equilateral
   
-  elsif s1 == s2 || s1 == s3 || s2 == s3
-  self.kind = :isosceles
+    elsif s1 == s2 || s1 == s3 || s2 == s3
+      self.kind = :isosceles
   
- else 
-   self.kind = :scalene
+    else 
+      self.kind = :scalene
   
     end   
   end
